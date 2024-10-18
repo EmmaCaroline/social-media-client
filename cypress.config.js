a9141425a44645edbed1292d7a0585173e12a7a5
EmmaCaroline <emma-caroline@hotmail.com>
2024-10-18 21:07:22 +0200

    Starting point for the e2e cypress login test

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,10 @@
-/*const { defineConfig } = require('cypress'); Bruke denne eller den under? */
-import { defineConfig } from 'cypress';
+/* eslint-disable no-unused-vars */
+const { defineConfig } = require('cypress');
+/*import { defineConfig } from 'cypress'; Use this or the one above?*/
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(/*on, config REMOVE COMMENT SIGNS LATER (on and config should be there) */) {
+    setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
