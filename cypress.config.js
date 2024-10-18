cdce5f947c699068b57dfe1f299acca3865e649f
EmmaCaroline <emma-caroline@hotmail.com>
2024-10-18 07:21:05 +0200

    Commented out code to use later

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
+    setupNodeEvents(/*on, config REMOVE COMMENT SIGNS LATER (on and config should be there) */) {
       // implement node event listeners here
     },
   },
