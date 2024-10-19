fc7bcb9e9c1cdc51c0a0733b3d54189e7ae0013c
EmmaCaroline <emma-caroline@hotmail.com>
2024-10-19 19:56:28 +0200

    Added cypress e2e test for 'ii.	The user cannot submit the login form with invalid credentials and is shown a message'

--- cypress/e2e/3-testing/login.form.cy.js
+++ cypress/e2e/3-testing/login.form.cy.js
@@ -3,7 +3,7 @@ describe('Login function', () => {
     cy.visit('/');
   });
 
-  it('should allow a valid, registered user to log in with the login form', () => {
+  it('should allow a registered user to log in with the login form with valid credentials', () => {
     cy.showLoginForm();
     cy.loginFakeProfile();
     cy.isLoggedIn();
